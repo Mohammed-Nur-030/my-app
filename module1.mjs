804ba090a6647173dc12da8ef7ae49264c77ebd7
Mohammed-Nur-030 <126596779+Mohammed-Nur-030@users.noreply.github.com>
2023-03-17 17:42:51 +0530

    End of video6

---
+++ module1.mjs
@@ -0,0 +1,10 @@
+const a = "Harry";
+const b = "Nur";
+const c = "Waseem";
+const d = "Naqui";
+
+
+export default b;
+export  {a};
+export  {c};
+export  {d};
---
+++ module2.mjs
@@ -0,0 +1,6 @@
+// import ui from './module1.mjs';
+import anyRandomName , {a,c,d} from './module1.mjs';
+console.log(anyRandomName);
+console.log(a);
+console.log(c);
+console.log(d);
