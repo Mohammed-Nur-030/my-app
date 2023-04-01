c55467d923040df86196d4e7bf741e287f49ebc5
Mohammed-Nur-030 <126596779+Mohammed-Nur-030@users.noreply.github.com>
2023-04-01 06:25:35 +0530

    lecture-16 completed
    Added reacr-router

--- src/App.js
+++ src/App.js
@@ -8,74 +8,95 @@ import { useState } from 'react';
 import React from 'react'
 import Alert from './components/Alert';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  
+  Link,
 
- 
+} from 'react-router-dom'
+
+
+
 
 
 function App() {
 
-  const [mode,setMode]=useState('light');
+  const [mode, setMode] = useState('light');
 
   const [alert, setAlert] = useState(null)
-    const showAlert = (message,type) => {
+  const showAlert = (message, type) => {
 
-      setAlert({
-        msg : message,
-        type : type
-      })
+    setAlert({
+      msg: message,
+      type: type
+    })
 
-      setTimeout(() => {
-        setAlert(null);
-      }, 2000);
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+
+  }
+
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark Mode has been enabled", "success");
+      document.title = 'TextUtils-DarkMode';
+      // setInterval(() => {
+      //   document.title='TextUtils-is Amazing';
+
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title=' Install TextUtils now';
+
+      // }, 1500);
+
+
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      document.title = 'TextUtils-LightMode';
+      showAlert("Light Mode has been enabled", "success");
 
     }
 
-
- const toggleMode = ()=>{
-  if (mode==='light') {
-    setMode('dark');
-    document.body.style.backgroundColor ='#042743';
-    showAlert("Dark Mode has been enabled", "success");
-    document.title='TextUtils-DarkMode';
-    // setInterval(() => {
-    //   document.title='TextUtils-is Amazing';
-      
-    // }, 2000);
-    // setInterval(() => {
-    //   document.title=' Install TextUtils now';
-      
-    // }, 1500);
-    
-    
   }
-  else{
-    setMode('light');
-    document.body.style.backgroundColor='white';
-    document.title='TextUtils-LightMode';
-    showAlert("Light Mode has been enabled", "success");
-   
-  }
-
-}
 
   return (
-    
 
 
-     <>
-     
-      <Navbar  title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
+
+    <>
+
+      <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
 
-        <Alert  alert={alert}/>
+        <Alert alert={alert} />
 
-      <TextForm heading="Enter the text to analyse Below" mode={mode}  showAlert={showAlert}/>
-      {/* <About mode={mode} btnText={btnText}></About> */}
-    
+
+        <Routes>
+          <Route path="/about" element={<About mode={mode}   showAlert={showAlert} />} />
+          {/* <Route exact path="/about" element={<About mode={mode}   showAlert={showAlert} />}  ---> this is better /> */}
+          
+
+          <Route path="/" element={<TextForm heading="Enter the text to analyse Below" mode={mode} showAlert={showAlert} />} />
+
+
+        </Routes>
+
+
+
+        
+
       </div>
 
 
-     </>
+    </>
 
 
 
@@ -83,10 +104,10 @@ function App() {
 
 
 
-    
+
   );
   // while returnting you sshould return a single tag and all other tags should be childrenv of returning tag.....but if you want sibling tags also then make ---------->>>>>   <>..all tags inside this </>
-} 
+}
 // the about written code is jsx---(html hi hain jisne JAvascript ka mukut pehna huwa hai)
 
 export default App;
